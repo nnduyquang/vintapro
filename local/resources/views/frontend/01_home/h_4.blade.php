fd7bdfdb47464cdea120777d3e8ea68c2034c67d
nnduyquang@gmail.com <bunvodich>
2019-07-08 15:57:50 +0700

    ok

--- local/resources/views/frontend/01_home/h_4.blade.php
+++ local/resources/views/frontend/01_home/h_4.blade.php
@@ -1,5 +1,5 @@
 <div id="h_4">
-    <h5>Tin Tức  <span>kế toán</span></h5>
+    <h5>Tin Tức <span>kế toán</span></h5>
     <div class="container">
         <div class="row">
             @foreach($data['h4_news'] as $key=>$item)
@@ -19,6 +19,197 @@
                     </div>
                 </div>
             @endforeach
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-4 mb-2">
+        <div class="one-item" style="border:1px solid grey">
+            <h3 style="background: linear-gradient(270deg, #ec0b00 0%, #fb6016 51%, #ff881e 75%);text-align: center;padding: 10px 0;color: white;font-weight: bolder;text-transform: uppercase;">
+                DT300 </h3>
+
+            <div style="
+    text-align: center;
+    border-bottom: 1px solid gray;
+    padding: 10px;
+">300 hóa đơn
+            </div>
+
+            <div style="
+    text-align: center;
+    font-weight: bold;
+    padding: 10px 0;
+    font-size: 24px;
+">297.000
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="one-item" style="border:1px solid grey">
+            <h3 style="background: linear-gradient(270deg, #ec0b00 0%, #fb6016 51%, #ff881e 75%);text-align: center;padding: 10px 0;color: white;font-weight: bolder;text-transform: uppercase;">
+                DT500  </h3>
+
+            <div style="
+    text-align: center;
+    border-bottom: 1px solid gray;
+    padding: 10px;
+">500 hóa đơn
+            </div>
+
+            <div style="
+    text-align: center;
+    font-weight: bold;
+    padding: 10px 0;
+    font-size: 24px;
+">410.000
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="one-item" style="border:1px solid grey">
+            <h3 style="background: linear-gradient(270deg, #ec0b00 0%, #fb6016 51%, #ff881e 75%);text-align: center;padding: 10px 0;color: white;font-weight: bolder;text-transform: uppercase;">
+                DT1.000 </h3>
+
+            <div style="
+    text-align: center;
+    border-bottom: 1px solid gray;
+    padding: 10px;
+">1.000 hóa đơn
+            </div>
+
+            <div style="
+    text-align: center;
+    font-weight: bold;
+    padding: 10px 0;
+    font-size: 24px;
+">640.000
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="one-item" style="border:1px solid grey">
+            <h3 style="background: linear-gradient(270deg, #ec0b00 0%, #fb6016 51%, #ff881e 75%);text-align: center;padding: 10px 0;color: white;font-weight: bolder;text-transform: uppercase;">
+                DT2.000 </h3>
+
+            <div style="
+    text-align: center;
+    border-bottom: 1px solid gray;
+    padding: 10px;
+">2.000 hóa đơn
+            </div>
+
+            <div style="
+    text-align: center;
+    font-weight: bold;
+    padding: 10px 0;
+    font-size: 24px;
+">960.000
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="one-item" style="border:1px solid grey">
+            <h3 style="background: linear-gradient(270deg, #ec0b00 0%, #fb6016 51%, #ff881e 75%);text-align: center;padding: 10px 0;color: white;font-weight: bolder;text-transform: uppercase;">
+                DT3.000 </h3>
+
+            <div style="
+    text-align: center;
+    border-bottom: 1px solid gray;
+    padding: 10px;
+">3.000 hóa đơn
+            </div>
+
+            <div style="
+    text-align: center;
+    font-weight: bold;
+    padding: 10px 0;
+    font-size: 24px;
+">1.350.000
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="one-item" style="border:1px solid grey">
+            <h3 style="background: linear-gradient(270deg, #ec0b00 0%, #fb6016 51%, #ff881e 75%);text-align: center;padding: 10px 0;color: white;font-weight: bolder;text-transform: uppercase;">
+                DT5.000 </h3>
+
+            <div style="
+    text-align: center;
+    border-bottom: 1px solid gray;
+    padding: 10px;
+">5.000 hóa đơn
+            </div>
+
+            <div style="
+    text-align: center;
+    font-weight: bold;
+    padding: 10px 0;
+    font-size: 24px;
+">2.050.000
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="one-item" style="border:1px solid grey">
+            <h3 style="background: linear-gradient(270deg, #ec0b00 0%, #fb6016 51%, #ff881e 75%);text-align: center;padding: 10px 0;color: white;font-weight: bolder;text-transform: uppercase;">
+                DT7.000  </h3>
+
+            <div style="
+    text-align: center;
+    border-bottom: 1px solid gray;
+    padding: 10px;
+">7.000 hóa đơn
+            </div>
+
+            <div style="
+    text-align: center;
+    font-weight: bold;
+    padding: 10px 0;
+    font-size: 24px;
+">2.730.000
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="one-item" style="border:1px solid grey">
+            <h3 style="background: linear-gradient(270deg, #ec0b00 0%, #fb6016 51%, #ff881e 75%);text-align: center;padding: 10px 0;color: white;font-weight: bolder;text-transform: uppercase;">
+                DT10.000 </h3>
+
+            <div style="
+    text-align: center;
+    border-bottom: 1px solid gray;
+    padding: 10px;
+">10.000 hóa đơn
+            </div>
+
+            <div style="
+    text-align: center;
+    font-weight: bold;
+    padding: 10px 0;
+    font-size: 24px;
+">3.400.000
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="one-item" style="border:1px solid grey">
+            <h3 style="background: linear-gradient(270deg, #ec0b00 0%, #fb6016 51%, #ff881e 75%);text-align: center;padding: 10px 0;color: white;font-weight: bolder;text-transform: uppercase;">
+                DT20.000 </h3>
+
+            <div style="
+    text-align: center;
+    border-bottom: 1px solid gray;
+    padding: 10px;
+">20.000 hóa đơn
+            </div>
+
+            <div style="
+    text-align: center;
+    font-weight: bold;
+    padding: 10px 0;
+    font-size: 24px;
+">5.800.000
+            </div>
         </div>
     </div>
 </div>
